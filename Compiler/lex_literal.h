2ecf0202f30f8318d8f381b4b0d6e34aa58fb379
刘予顺 <ys_37677@126.com>
2016-10-30 14:09:33 +0800

    tokens

--- Compiler/lex_literal.h
+++ Compiler/lex_literal.h
@@ -2,19 +2,19 @@
 #define lex_literal_h
 
 #include <iostream>
+#include <algorithm>
 
 namespace lexical
 {
     using pchar = char*;
-    using func = pchar(pchar);
     
-    template <func f>
+    template <class f>
     inline pchar invoke(pchar ch)
     {
-        return (*ch != 0) ? f(ch) : nullptr;
+        return (*ch != 0) ? f::w(ch) : nullptr;
     }
     
-    template <size_t l, func f, func ...s>
+    template <size_t l, class f, class ...s>
     struct seq
     {
         inline static pchar w(pchar ch)
@@ -26,7 +26,7 @@ namespace lexical
         }
     };
     
-    template <func f>
+    template <class f>
     struct seq<1, f>
     {
         inline static pchar w(pchar ch)
@@ -53,17 +53,16 @@ namespace lexical
         }
     };
     
-    template <size_t l, func f, func ...fs>
+    template <size_t l, class f, class ...fs>
     struct _or
     {
         inline static pchar w(pchar ch)
         {
-            pchar c = invoke<f>(ch);
-            return max(c, invoke<_or<l - 1, fs...>::w>(ch));
+            return std::max(invoke<f>(ch), invoke<_or<l - 1, fs...> >(ch));
         }
     };
     
-    template <func f>
+    template <class f>
     struct _or<1, f>
     {
         inline static pchar w(pchar ch)
@@ -72,7 +71,7 @@ namespace lexical
         }
     };
     
-    template <size_t l, size_t m, func f>
+    template <size_t l, size_t m, class f>
     struct _for
     {
         inline static pchar w(pchar ch)
@@ -97,18 +96,16 @@ namespace lexical
         }
     };
     
-    template <size_t l, func f, func ...s>
+    template <size_t l, class f, class ...s>
     struct _and
     {
         inline static pchar w(pchar ch)
         {
-            ch = invoke<f>(ch);
-            pchar c  = _and<l - 1, s...>::w(ch);
-            return min(ch, c);
+            return std::min(invoke<f>(ch), _and<l - 1, s...>::w(ch));
         }
     };
     
-    template <func f>
+    template <class f>
     struct _and<1, f>
     {
         inline static pchar w(pchar ch)
@@ -117,7 +114,7 @@ namespace lexical
         }
     };
     
-    template <func f>
+    template <class f>
     struct _not
     {
         inline static pchar w(pchar ch)
@@ -127,83 +124,122 @@ namespace lexical
         }
     };
     
-    template <func f>
+    template <class f>
     using any = _for<0, 0xFFFFFFFF, f>;
-    template <func f>
+    template <class f>
     using opt = _for<0, 1, f>;
-    template <func f>
+    template <class f>
     using orm = _for<1, 0xFFFFFFFF, f>;
-    template <size_t t, func f>
+    template <size_t t, class f>
     using tim = _for<t, t, f>;
     
     using dec = _in<'0', '9'>;
     using oct = _in<'0', '7'>;
-    using hex = _or<3, _in<'0', '9'>::w, _in<'a', 'f'>::w, _in<'A', 'F'>::w>;
-    using sign = _or<2, _is<'+'>::w, _is<'-'>::w>;
+    using hex = _or<3, _in<'0', '9'>, _in<'a', 'f'>, _in<'A', 'F'> >;
+    using sign = _or<2, _is<'+'>, _is<'-'> >;
     using universal_character_name =
-    seq<2, _is<'\\'>::w,_or<2,seq<2, _is<'u'>::w, tim<4, hex::w>::w>::w,
-    seq<2, _is<'U'>::w, tim<8, hex::w>::w>::w>::w>;
+    seq<2, _is<'\\'>,_or<2,seq<2, _is<'u'>, tim<4, hex> >,
+    seq<2, _is<'U'>, tim<8, hex> > > >;
     
     namespace preprocessing_token
     {
-        enum class tokens : size_t
+        enum tokens
         {
-            header_name,
-            identifier,
-            pp_number,
-            character_literal,
-            user_defined_character_literal,
-            string_literal,
-            user_defined_string_literal,
-            preprocessing_op_or_punc,
-            others
+            t_header_name,
+            t_identifier,
+            t_pp_number,
+            t_character_literal,
+            t_user_defined_character_literal,
+            t_string_literal,
+            t_user_defined_string_literal,
+            t_preprocessing_op_or_punc,
+            t_others
         };
         
         //header name token
-        using no_new_line = _not<_is<'\n'>::w>;
-        using h_char = _and<2, no_new_line::w, _not<_is<'>'>::w>::w>;
-        using h_char_sequence = orm<h_char::w>;
-        using q_char = _and<2, no_new_line::w, _not<_is<'"'>::w>::w>;
-        using q_char_sequence = orm<q_char::w>;
+        using no_new_line = _not<_is<'\n'> >;
+        using h_char = _and<2, no_new_line, _not<_is<'>'> > >;
+        using h_char_sequence = orm<h_char>;
+        using q_char = _and<2, no_new_line, _not<_is<'"'> > >;
+        using q_char_sequence = orm<q_char>;
         using header_name =
-        _or<2, seq<3, _is<'<'>::w, h_char_sequence::w, _is<'>'>::w>::w,
-        seq<3, _is<'"'>::w, q_char_sequence::w, _is<'"'>::w>::w>;
+        _or<2, seq<3, _is<'<'>, h_char_sequence, _is<'>'> >,
+        seq<3, _is<'"'>, q_char_sequence, _is<'"'> > >;
         
         //identifiers
-        using nondigit = _or<3, _in<'a', 'z'>::w, _in<'A', 'Z'>::w, _is<'_'>::w>;
-        using identifier_nondigit = _or<2, nondigit::w, universal_character_name::w>;
+        using nondigit = _or<3, _in<'a', 'z'>, _in<'A', 'Z'>, _is<'_'> >;
+        using identifier_nondigit = _or<2, nondigit, universal_character_name>;
         using identifier =
-        seq<2, identifier_nondigit::w, any<_or<2, identifier_nondigit::w, dec::w>::w>::w>;
+        seq<2, identifier_nondigit, any<_or<2, identifier_nondigit, dec> > >;
+        
+        //pp number
+        using pp_number = seq<3, opt<_is<'.'> >, dec, any<_or<7,
+        dec, seq<2, _is<'\''>, dec>, seq<2, _is<'\''>, nondigit>, 
+        identifier_nondigit, seq<2, _is<'e'>, sign>, 
+        seq<2, _is<'E'>, sign>, _is<'.'> > > >;
+        
+        //character
+        using hexadecimal_escape_sequence = seq<3, _is<'\\'>, _is<'x'>, orm<hex> >;
+        using octal_escape_sequence = seq<2, _is<'\\'>, _for<1, 3, oct> >;
+        using simple_escape_sequence = seq<2, _is<'\\'>, _or<11, 
+        _is<'\''>, _is<'\"'>, _is<'\?'>, _is<'\\'>, _is<'a'>, _is<'b'>,
+        _is<'f'>, _is<'n'>, _is<'r'>, _is<'t'>, _is<'v'> > >;
+        using escape_sequence = _or<3, hexadecimal_escape_sequence,
+        octal_escape_sequence, simple_escape_sequence>;
+        using c_char = _or<3, 
+        _and<3, no_new_line, _not<_is<'\''> >, _not<_is<'\\'> > >,
+        escape_sequence, universal_character_name>;
+        using c_char_sequence = orm<c_char>;
+        using character_literal = seq<4, 
+        opt<_or<3, _is<'u'>, _is<'U'>, _is<'L'> > >, 
+        _is<'\''>, c_char_sequence, _is<'\''> >;
+        
+        //ud character
+        using user_defined_character_literal = seq<2, character_literal, identifier>;
+        
+        //string literal
+        using s_char = _or<3, 
+        _and<3, no_new_line, _not<_is<'\"'> >, _not<_is<'\\'> > >,
+        escape_sequence, universal_character_name>;
+        using s_char_sequence = orm<s_char>;
+        using d_char = _not<_or<8, _is<'\n'>, _is<'\t'>, _is<'\v'>, _is<'\f'>,
+        _is<'('>, _is<')'>, _is<' '>, _is<'\\'> > >;
+        using d_char_sequence = orm<d_char>;
+        pchar str_comp(pchar b1, pchar e1, pchar b2)       
+        {
+            for (; b1 < e1; ++b1, ++b2)
+                if ((*b1 != *b2) || (*b2 == 0))
+                    return nullptr;
+            return b2;
+        }
+        struct raw_str_content
+        {
+            static inline pchar w(pchar ch)
+            {
+                pchar dchar = invoke<opt<d_char_sequence> >(ch);
+                pchar i = invoke<_is<'('> >(dchar);
+                if (i == nullptr) return nullptr;
+                for (; *i != 0; ++i)
+                    if (*i == ')')
+                    {
+                        pchar e = str_comp(ch, dchar, i + 1);
+                        if (e)
+                            return e;
+                    }
+                return nullptr;
+            }
+        };
+        using raw_string = seq<3, _is<'\"'>, raw_str_content, _is<'\"'> >;
+        using encoding_prefix = _or<4, seq<2, _is<'u'>, _is<'8'> >,
+        _is<'U'>, _is<'u'>, _is<'L'> >;
+        using string_literal = seq<2, opt<encoding_prefix>, _or<2,
+        seq<3, _is<'\"'>, opt<s_char_sequence>, _is<'\"'> >,
+        seq<2, _is<'R'>, raw_string> > >;
+        
+        //ud string
+        using user_defined_string_literal = seq<2, string_literal, identifier>;
     }
     
-    using digit_sequence = orm<dec::w>;
-    using fractional_constant =
-    _or<2, seq<2, digit_sequence::w, _is<'.'>::w>::w,
-    seq<3, opt<digit_sequence::w>::w, _is<'.'>::w, digit_sequence::w>::w>;
-    using exponent =
-    seq<3, _or<2, _is<'E'>::w, _is<'e'>::w>::w, opt<sign::w>::w, digit_sequence::w>;
-    using floating_suffix =
-    _or<4, _is<'f'>::w, _is<'F'>::w,_is<'l'>::w, _is<'L'>::w>;
-    using floating_literal =
-    seq<2, _or<2, seq<2, fractional_constant::w, opt<exponent::w>::w>::w,
-    seq<2, digit_sequence::w, exponent::w>::w>::w,
-    opt<floating_suffix::w>::w>;
-    
-    using nonzero = _in<'1', '9'>;
-    using decimal_literal = seq<2, nonzero::w, any<dec::w>::w>;
-    using octal_literal = seq<2, _is<'0'>::w, any<oct::w>::w>;
-    using hexadecimal_literal = seq<3, _is<'0'>::w,
-    _or<2, _is<'x'>::w, _is<'X'>::w>::w, any<hex::w>::w>;
-    using unsigned_suffix = _or<2, _is<'u'>::w, _is<'U'>::w>;
-    using long_suffix = _or<2, _is<'l'>::w, _is<'L'>::w>;
-    using long_long_suffix = tim<2, _or<2, _is<'l'>::w, _is<'L'>::w>::w>;
-    using unified_long_suffix =_or<2, long_suffix::w, long_long_suffix::w>;
-    using integer_suffix =
-    _or<2,seq<2, unsigned_suffix::w, opt<unified_long_suffix::w>::w>::w,
-    seq<2, unified_long_suffix::w, unsigned_suffix::w>::w>;
-    using integer_literal =
-    seq<2,_or<3, decimal_literal::w, octal_literal::w, hexadecimal_literal::w>::w,
-    opt<integer_suffix::w>::w>;
 }
 
 #endif
